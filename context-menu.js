9a575993fa05f32bf9b64201150f7636c5fcc01e
Max Vyznyuk <mvyznyuk@gmail.com>
2016-07-16 03:13:04 +0300

    Write ssome style for context menu, create function to show-hide context menu

--- context-menu.js
+++ context-menu.js
@@ -15,6 +15,31 @@ window.addEventListener('load', function() {
       onClick: "clickHandler",
       disabled: true,
       submenu: false
+    },{
+      title: 'item 2',
+      onClick: "clickHandler",
+      disabled: false,
+      submenu: [{
+        title: 'sub item 1',
+        onClick: "clickHandler",
+        disabled: true,
+        submenu: false
+      },{
+        title: 'sub item 1',
+        onClick: "clickHandler",
+        disabled: true,
+        submenu: false
+      },{
+        title: 'sub item 1',
+        onClick: "clickHandler",
+        disabled: true,
+        submenu: false
+      },]
+    },{
+      title: 'item 2',
+      onClick: "clickHandler",
+      disabled: true,
+      submenu: false
     }]
   }
 
@@ -27,10 +52,63 @@ window.addEventListener('load', function() {
 
   console.log(targetElement);
 
-  targetElement.addEventListener('click', openContextMenu, false);
+  targetElement.addEventListener('contextmenu', openContextMenu, false);
   var contextMenuTemplateElement = document.createElement('div');
+  contextMenuTemplateElement.className = "context-menu";
+  contextMenuTemplateElement.id = 'contextMenu';
+  insertMenuItems.call(contextMenuTemplateElement, menuConfig.items);
+
 
   function openContextMenu(event) {
+    event.preventDefault();
     console.log(event.x, event.y, document.body.clientHeight, document.body.clientWidth);
+
+    contextMenuTemplateElement.style.top = event.y + 'px';
+    contextMenuTemplateElement.style.left = event.x + 'px';
+    document.body.appendChild(contextMenuTemplateElement);
+    window.addEventListener('click', needToHideContext, false);
   }
 });
+
+function needToHideContext(event) {
+  // if (!event.target.parentElement) {
+  if (event.target.id !== 'contextMenu') {
+    document.body.removeChild(document.getElementById('contextMenu'));
+  }
+}
+
+function insertMenuItems(items, isSubmenu) {
+  var itemsWrapper = document.createElement('div');
+  if (isSubmenu) {
+    itemsWrapper.className = 'submenu'
+  }
+
+  items.forEach(function(item, index) {
+    var itemElement = document.createElement('div');
+    var textWrapper = document.createElement('span');
+    textWrapper.innerText = item.title;
+    itemElement.appendChild(textWrapper);
+    itemElement.className = 'context-menu-item';
+    itemElement.addEventListener('click', window[item.onClick], false);
+
+    if (item.disabled) {
+      itemElement.className += ' is-disabled';
+      itemElement.removeEventListener('click', window[item.onClick]);
+    }
+
+    if (item.submenu) {
+      itemElement.className += ' has-sub-items submenu-hidden';
+      itemElement.removeEventListener('click', window[item.onClick]);
+      // itemElement.addEventListener('click', openContextMenu())
+      insertMenuItems.call(itemElement, item.submenu, true);
+    }
+
+    itemsWrapper.appendChild(itemElement);
+  });
+
+  this.appendChild(itemsWrapper);
+}
+
+function clickHandler(event) {
+  console.log(event);
+}
